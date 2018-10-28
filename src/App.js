9ecbe1781350f336c2602f7c44c7a39fbe6a060f
wrk <wrks@mac.com>
2018-10-28 21:42:03 +0100

    added Redux

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React, { Component } from "react";
+import "./App.css";
+import { Provider } from "react-redux";
+
 import Posts from "./components/Posts";
 import PostForm from "./components/PostForm";
-import "./App.css";
+
+import store from "./store";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <PostForm />
-        <hr />
-        <Posts />
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <PostForm />
+          <hr />
+          <Posts />
+        </div>
+      </Provider>
     );
   }
 }
